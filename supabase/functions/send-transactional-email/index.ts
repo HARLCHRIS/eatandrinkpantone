fc4b77261805480b624072ff03ee63667a3264f8
Harlen <tchedjiharlen13@gmail.com>
2026-09-20 07:22:30 -0700

    CORS sur l'Edge Function

--- supabase/functions/send-transactional-email/index.ts
+++ supabase/functions/send-transactional-email/index.ts
@@ -8,7 +8,7 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://eatandrink.pantoneafrica.com',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
